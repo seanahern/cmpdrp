ba8bd724407514f33ece5b0a48e3a27d9e4306e0
sean ahern <seanpahern@gmail.com>
2016-05-15 18:57:11 -0700

    Moved to src => dist structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,17 +2,25 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     sourcemaps = require('gulp-sourcemaps'),
     gutil = require('gulp-util'),
+    minify = require('gulp-minify'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  gulp.src('css/cmpdrp.scss')
+  gulp.src('src/css/cmpdrp.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', gutil.log))
 //  .pipe(cleanCSS().on('error'), gutil.log)
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', function() {
+  gulp.src('src/js/cmpdrp.js')
+    .pipe(minify().on('error', gutil.log))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', function() {
-  gulp.watch('css/cmpdrp.scss', ['sass']);
+  gulp.watch('src/css/cmpdrp.scss', ['sass']);
+  gulp.watch('src/js/cmpdrp.js', ['js']);
 });
